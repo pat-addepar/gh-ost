bf3bad2ed6cd31170403af4210a3782e76161a9b
Patrick DePippo <patrick.depippo@addepar.com>
2020-10-27 17:08:41 -0400

    fix: make instead of broken install script

--- script/bootstrap-comet.sh
+++ script/bootstrap-comet.sh
@@ -1,28 +1,30 @@
-#!/bin/bash
-
+#!/bin/sh
 cd ~
 wget https://golang.org/dl/go1.14.10.linux-amd64.tar.gz -O go1.14.10.src.tar.gz
 tar xvf go1.14.10.src.tar.gz
 
-#sudo chown -R root:root ./go
-#sudo mv go /usr/local
+sudo chown -R root:root ./go
+sudo mv go /usr/local
 
 # Update paths
-# echo "export GOROOT=\$HOME/go"
-echo "export GOPATH=\$HOME/go" >> $HOME/.profile
-echo "export PATH=\$PATH:\$GOPATH/bin" >> $HOME/.profile
+echo "export GOROOT=/usr/local/go" >> $HOME/.profile
+echo "export GOPATH=\$HOME/work" >> $HOME/.profile
+echo "export PATH=\$PATH:\$GOROOT/bin" >> $HOME/.profile
 . ~/.profile
+
+mkdir $HOME/work
 
 # Install go dep
 go get -d -u github.com/golang/dep
 cd $(go env GOPATH)/src/github.com/golang/dep
 DEP_LATEST=$(git describe --abbrev=0 --tags)
 git checkout $DEP_LATEST
-GOBIN=$GOPATH/bin bash ./install.sh
+make build
+sudo mv dep /usr/local/bin/
 cd ~/
 
-mkdir -p $HOME/go/src/github.com/pat-addepar/
-cd $HOME/go/src/github.com/pat-addepar/
+mkdir -p $HOME/work/src/github.com/pat-addepar/
+cd $HOME/work/src/github.com/pat-addepar/
 git clone https://github.com/pat-addepar/gh-ost && cd gh-ost
 
 # Install Deps
